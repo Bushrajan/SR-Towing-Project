3375492590fa3995b36b533333c1b73ef0537508
Bushrajan <156935628+Bushrajan@users.noreply.github.com>
2024-12-16 16:13:31 +0500

    update

--- app/swiper/SwiperSlider.tsx
+++ app/swiper/SwiperSlider.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useRef, useState } from 'react'
+import React from 'react'
 import Image from 'next/image'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Autoplay } from 'swiper/modules'
